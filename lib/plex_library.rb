0eee400675fd3fab7818c307c2e7a88d2e423260
whoismezero <whoismezero@gmail.com>
2013-06-07 18:21:12 -0400

    dfgh

---
+++ lib/plex_library.rb
@@ -0,0 +1,224 @@
+require 'cgi'
+require 'open-uri'
+require 'plex_show'
+require 'plex_season'
+require 'plex_episode'
+require 'plex_ondeck'
+TV = "com.plexapp.agents.thetvdb"
+MOVIES = "com.plexapp.agents.imdb"
+
+
+class PlexLibrary
+  def initialize(host, port, tv_index, movie_index, player = nil)
+    @host     = host
+    @port     = port
+#    @tv_index = tv_index
+    @movie_index = movie_index
+    @player   = player.nil? ? host : player
+    @indexes = {}
+    @indexes["#{TV}"] = []
+    if (tv_index == "auto")
+       doc = xml_doc_for_path("/library/sections")
+       doc.elements.each('MediaContainer/Directory') do |ele|
+          num = ele.attribute("key").value
+          agent = ele.attribute("agent").value
+          if (!@indexes.include?("#{agent}"))
+             @indexes["#{agent}"] = []
+          end
+             @indexes["#{agent}"] << "#{num}"
+       end
+    else
+       @indexes["#{TV}"] << "#{tv_index}"
+    end
+	@indexes["#{MOVIES}"] = []
+	@indexes["#{MOVIES}"] << "#{movie_index}"
+  end
+  
+  def base_path
+    "http://#{@host}:#{@port}"
+  end
+  
+  def xml_doc_for_path(path)
+    uri = "#{base_path}#{path}"
+    xml_data = open(uri).read
+    doc = REXML::Document.new(xml_data)
+  end
+  
+# all_shows and all_ondeck should be colapsed into one method with a variable for path.
+
+  def all_shows
+    shows = []
+    @indexes[TV].each do |tvindex|
+       doc = xml_doc_for_path("/library/sections/#{tvindex}/all")
+
+       doc.elements.each('MediaContainer/Directory') do |ele|
+         shows << PlexShow.new(ele.attribute("key").value, ele.attribute("title").value)
+       end
+    end
+    return shows
+  end
+
+  def all_ondeck
+    ondeck_shows = []
+    @indexes[TV].each do |tvindex|
+       doc = xml_doc_for_path("/library/sections/#{tvindex}/onDeck")
+
+       doc.elements.each('MediaContainer/Video') do |ele|
+         ondeck_shows << PlexOndeck.new(ele.attribute("key").value, ele.attribute("title").value, ele.attribute("grandparentTitle").value)
+       end
+    end
+    return ondeck_shows
+  end
+
+  def show_seasons(show)
+    
+    if(show.key != nil)
+      uri = "http://#{@host}:#{@port}#{show.key}"
+      
+      xml_data = open(uri).read
+      doc = REXML::Document.new(xml_data)
+      seasons = []
+      
+      doc.elements.each('MediaContainer/Directory') do |ele|
+        if(ele.attribute("key").value !~ /allLeaves/)
+          seasons << PlexSeason.new(ele.attribute("key").value, ele.attribute("title").value, ele.attribute("index").value)
+        end
+      end
+      
+      return seasons
+    end
+    
+  end
+  
+  def show_episodes(show)
+    if(show != nil && show.key != nil)
+      doc = xml_doc_for_path(show.key)
+      episodes = []
+      
+      key = nil
+      
+      doc.elements.each('MediaContainer/Directory') do |ele|
+        if(ele.attribute("key").value =~ /allLeaves/)
+          key = ele.attribute("key")
+        end
+      end
+      
+      if(key == nil && doc.elements.size > 0)
+        doc.elements.each('MediaContainer/Directory') do |ele|
+          key = ele.attribute("key")
+        end
+      end
+      
+      episodes_doc = xml_doc_for_path(key)
+      
+      episodes_doc.elements.each('MediaContainer/Video') do |video_element|
+        parentIndex = video_element.attribute("parentIndex") ? video_element.attribute("parentIndex").value : 1
+        episodes << PlexEpisode.new(video_element.attribute("key").value, video_element.attribute("title").value, parentIndex, video_element.attribute("index").value)
+      end
+      
+      return episodes
+    end
+  end
+
+  def find_ondeck_show(title)
+    title.gsub!(/^The\s+/, "")
+    splitted = title.split(" ").join("|")
+    shows = all_ondeck
+    show = shows.detect {|s| s.gptitle.match(/#{splitted}/i)}
+    return show
+  end
+  
+  def find_show(title)
+    title.gsub!(/^The\s+/, "")
+    splitted = title.split(" ").join("|") 
+    shows = all_shows    
+    shows.detect {|s| s.title.match(/#{splitted}/i)}
+  end
+  
+  def find_episode(show, season_index, episode_index)
+    if(show != nil)
+      episodes = show_episodes(show)
+      episodes.find {|e| e.episode_index == episode_index && e.season_index == season_index}
+    end
+  end
+  
+  def has_many_seasons?(show)
+    if(show == nil)
+      return false
+    end
+    
+    episodes = show_episodes(show)    
+    season = nil
+    
+    episodes.each do |ep|
+      if(season != nil && season != ep.season_index)
+        return true
+      end
+      
+      season = ep.season_index
+    end
+    
+    return false
+  end
+  
+  def latest_season_index(show)
+    if show == nil then return nil end
+    show_episodes(show).sort.last.season_index
+  end
+  
+  def latest_episode(show)
+    if show == nil then return nil end
+    show_episodes(show).sort.last
+  end
+  
+    def all_movies
+    movies = []
+    @indexes[MOVIES].each do |movieindex|
+       doc = xml_doc_for_path("/library/sections/#{movieindex}/all")
+
+       doc.elements.each('MediaContainer/Directory') do |ele|
+         movies << PlexShow.new(ele.attribute("key").value, ele.attribute("title").value)
+       end
+    end
+    return movies
+  end
+  
+  def find_movie(title)
+    title.gsub!(/^The\s+/, "")
+    splitted = title.split(" ").join("|") 
+    movies = all_movies    
+    movies.detect {|s| s.title.match(/#{splitted}/i)}
+  end
+
+  def all_ondeck_movies
+    ondeck_movies = []
+	@indexes[MOVIES].each do |movieindex|
+       doc = xml_doc_for_path("/library/sections/#{movieindex}/onDeck")
+
+       doc.elements.each('MediaContainer/Video') do |ele|
+         ondeck_movies << PlexOndeck.new(ele.attribute("key").value, ele.attribute("title").value, ele.attribute("grandparentTitle"))
+       end
+    end
+    return ondeck_movies
+  end
+  
+  def find_ondeck_movie(title)
+    title.gsub!(/^The\s+/, "")
+    splitted = title.split(" ").join("|")
+    movies = all_ondeck_movies
+    movies = movies.detect {|s| s.title.match(/#{splitted}/i)}
+    return movies
+  end
+  
+  def play_media(key)
+    url_encoded_key = CGI::escape(key)
+    uri = "http://#{@host}:#{@port}/system/players/#{@player}/application/playMedia?key=#{url_encoded_key}&path=http://#{@host}:#{@port}#{key}"
+    
+    begin
+      open(uri).read
+    rescue OpenURI::HTTPError => err
+      puts "Cannot start playback on #{@player} - are you sure the Plex Player is running (#{err}) -> #{uri}"
+    end
+  end
+  
+end
---
+++ lib/siriproxy-plex.rb
@@ -0,0 +1,261 @@
+# Copyright (C) 2011 by Hjalti Jakobsson <hjalti@hjaltijakobsson.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'cora'
+require 'siri_objects'
+require 'rexml/document'
+require 'plex_library'
+
+#######
+# This is a very basic plugin for Plex but I plan on adding to it =)
+# Remember to configure the host and port for your Plex Media Server in config.yml in the SiriProxy dir or in ~/.siriproxy/ if you
+# are using the latest version of SiriProxy
+######
+
+class SiriProxy::Plugin::Plex < SiriProxy::Plugin
+  
+  def initialize(config)
+    @host = config["plex_host"]
+    @port = config["plex_port"]
+    @tv_index = config["plex_tv_index"]
+	@movie_index = config["plex_movie_index"]
+    @player = config["plex_player_host"].nil? ? config["plex_host"] : config["plex_player_host"]
+    @plex_library = PlexLibrary.new(@host, @port, @tv_index, @movie_index, @player)
+  end
+
+  listen_for /on deck tv shows/i do
+    ondeck_shows = @plex_library.all_ondeck()
+    if(!ondeck_shows.empty?)
+       say "On Deck shows are:"
+       ondeck_shows.each do |singleshow|
+         say "#{singleshow.gptitle}, #{singleshow.title}"
+       end 
+       response = ask "Which show would you like to watch?"
+       show = @plex_library.find_ondeck_show(response)
+       if(show != nil)
+         @plex_library.play_media(show.key)
+         say "Playing #{show.gptitle}, #{show.title}."
+       else
+         say "Sorry I couldn't find #{response} in the ondeck queue"
+       end 
+    else
+      say "Sorry I couldn't find anything in your onDeck queue"
+    end 
+    request_completed
+  end 
+  
+    listen_for /on deck movies/i do
+    ondeck_movies = @plex_library.all_ondeck_movies()
+    if(!ondeck_movies.empty?)
+       say "On Deck movies are:"
+       ondeck_movies.each do |singlemovie|
+         say "#{singlemovie.title}"
+       end 
+       response = ask "Which movie would you like to watch?"
+       movie = @plex_library.find_ondeck_movie(response)
+       if(movie != nil)
+         @plex_library.play_media(movie.key)
+         say "Playing #{movie.title}."
+       else
+         say "Sorry I couldn't find #{response} in the ondeck queue"
+       end 
+    else
+      say "Sorry I couldn't find anything in your onDeck queue"
+    end 
+    request_completed
+  end 
+  
+  listen_for /(play|playing) (the)? movie (.+)/i do |command, misc, some, next_movie|
+    movies = @plex_library.all_movies()
+	if(!movies.empty?)
+      movie = @plex_library.find_movie(next_movie)
+	  if(movie != nil)
+	    @plex_library.play_media(movie.key)
+        say "Playing #{movie.title}."
+	  else
+	    say "Sorry I couldn't find #{next_movie}."
+	  end
+	else
+	  say "Sorry I couldn't find any movies. #{movies}"
+	end
+	request_completed
+  end
+  
+  listen_for /(play|playing) (the)? next(.+) of (.+)/i do |command, misc, some, next_episode|
+    ondeck_shows = @plex_library.all_ondeck()
+    if(!ondeck_shows.empty?)
+       show = @plex_library.find_ondeck_show(next_episode)
+       if(show != nil)
+         @plex_library.play_media(show.key)
+         say "Playing #{show.gptitle}, #{show.title}."
+       else
+         say "Sorry I couldn't find #{next_episode} in the ondeck queue"
+       end 
+    else
+      say "Sorry I couldn't find anything in your onDeck queue"
+    end 
+    request_completed
+  end 
+  
+  listen_for /(play|playing) (the)? latest(.+) of(.+)/i do |command, misc, some, show|
+    play_latest_episode_of(show)
+    request_completed
+  end
+  
+  listen_for /(play|playing)(.+)/i do |command, show_title|
+
+    season_index = 1
+    show = @plex_library.find_show(show_title)
+
+    if(@plex_library.has_many_seasons?(show))
+      season_index = ask_for_season
+      episode_index = ask_for_episode
+    else
+      episode_index = ask_for_episode
+    end
+            
+    play_episode(show, episode_index, season_index)
+    
+    request_completed      
+  end
+  
+  listen_for /(play|playing) (.+)\sepisode (.+)/i do |command, first_match, second_match|
+    
+    show_title = first_match
+    
+    if(first_match.match(/(.+) season/))
+      show_title = $1
+    end
+    
+   show = @plex_library.find_show(show_title)    
+    season_index = match_number(first_match, "season")    
+    episode_index = match_number(second_match)
+    
+    #We need to match season in both first match and second
+    #play mythbusters episode 9 season 10 or
+    #play mythbusters season 10 episode 9
+    if(season_index == -1)
+      season = match_number(second_match)
+    end
+    
+    has_many_seasons = @plex_library.has_many_seasons?(show)
+    
+    if(season_index == -1 && has_many_seasons)
+      season_index = ask_for_season
+    elsif(season_index == -1 && !has_many_seasons)
+      season_index = 1
+    end
+    
+    if(show)
+      play_episode(show, episode_index, season_index)
+    else
+      show_not_found
+    end
+    
+    request_completed
+  end
+  
+  def ask_for_number(question)   
+    episode = nil
+    
+    while(response = ask(question))
+      
+      number = -1
+      
+      if(response =~ /([0-9]+\s*|one|two|three|four|five|six|seven|eight|nine|ten)/i)
+        number = $1
+        break
+      else
+        question = "I didn't get that, please state a number"
+      end
+    end
+    
+    if(number.to_i == 0)
+        number = map_siri_numbers_to_int(number)
+    end
+    
+    number.to_i
+  end
+  
+  def match_number(text, key = nil)
+    if(text.match(/#{key}\s*([0-9]+|one|two|three|four|five|six|seven|eight|nine|ten)/i))
+      
+      number = $1.to_i
+      
+      if(number == 0)
+        number = map_siri_numbers_to_int($1)
+      end
+      
+      return number
+    end
+    
+    return -1
+  end
+  
+  def ask_for_season
+    ask_for_number("Which season?")
+  end
+  
+  def ask_for_episode
+    ask_for_number("Which episode?")
+  end
+  
+  def play_episode(show, episode_index, season_index = 1)
+    
+    if(show != nil)
+      episode = @plex_library.find_episode(show, season_index, episode_index)
+      
+      if(episode)
+        @plex_library.play_media(episode.key)
+        say "Playing \"#{episode.title}\""
+      else
+        episode_not_found
+      end
+    else
+      show_not_found
+    end
+  end
+  
+  def show_not_found
+    say "I'm sorry but I couldn't find that TV show"
+  end
+  
+  def episode_not_found
+    say "I'm sorry but I couldn't find the episode you asked for"
+  end
+  
+  def map_siri_numbers_to_int(number)
+    ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"].index(number.downcase)
+  end
+  
+  def play_latest_episode_of(show_title)
+    show = @plex_library.find_show(show_title)
+    
+    episode = @plex_library.latest_episode(show)
+
+    if(episode != nil)
+      @plex_library.play_media(episode.key)
+      say "Playing \"#{episode.title}\""
+    else
+      episode_not_found
+    end
+  end
+  
+end
