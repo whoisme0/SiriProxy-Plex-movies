7377aa966162c6d96a8eb5e0588af7c581d718d5
whoismezero <whoismezero@gmail.com>
2013-09-14 23:46:16 -0400

    k,umjynhbgfrv

--- lib/siriproxy-plex.rb
+++ lib/siriproxy-plex.rb
@@ -363,6 +363,9 @@ class SiriProxy::Plugin::Plex < SiriProxy::Plugin
 	  if nextPlayer != nil
 		newIP = ask "What is the IP address of #{nextPlayer}#{63.chr}"
 		  if newIP != nil
+			newIP = newIP.split!(" dot ").join(".")
+			newIP = newIP.split!(" ").join(".")
+			say "debug: #{newIP}"
 			newPlayer(nextPlayer, newIP)
 			say "Okay, I added #{newPlayer} at #{@players[newPlayer]}."
 		  else
